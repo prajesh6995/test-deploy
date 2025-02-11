44c5b495f16f51bb7d441973d919d0c75038bc14
prajesh6995 <65954620+prajesh6995@users.noreply.github.com>
2025-02-11 17:53:07 +0530

    a

--- interservice-ecs.sh
+++ interservice-ecs.sh
@@ -11,7 +11,7 @@ CONTAINER_NAME_NPM="nginx-proxy-manager-container"
 IMAGE_URI_NGINX="nginx:latest"  # NGINX official image
 IMAGE_URI_NPM="jc21/nginx-proxy-manager:latest"  # Nginx Proxy Manager image
 PORT_NGINX=80
-PORT_NPM=80
+PORT_NPM=8080
 REGION="us-east-1"
 VPC_CIDR="10.0.0.0/16"
 SUBNET_CIDR1="10.0.1.0/24"
@@ -253,7 +253,7 @@ CREATED_RESOURCES["TG_NGINX"]=$TG_ARN_NGINX
 TG_ARN_NPM=$(aws elbv2 create-target-group \
   --name $TG_NAME_NPM \
   --protocol HTTP \
-  --port 80 \
+  --port 8080 \
   --vpc-id $VPC_ID \
   --target-type ip \
   --query 'TargetGroups[0].TargetGroupArn' \
@@ -266,7 +266,7 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Configuring health check for Nginx Proxy Ma
 aws elbv2 modify-target-group \
   --target-group-arn $TG_ARN_NPM \
   --health-check-path /login \
-  --health-check-port 80 \
+  --health-check-port 8080 \
   --matcher '{"HttpCode": "200,302,401"}'
 check_error "Failed to configure health check for Nginx Proxy Manager"
 
